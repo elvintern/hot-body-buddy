4886c00faba7564115e656a116598ee9cadb319b
elvintern <elvintern@gmail.com>
2022-11-08 23:48:32 +1300

    Delete preset code

--- client/apiClient.js
+++ client/apiClient.js
@@ -1,5 +1,5 @@
-import request from 'superagent'
+// import request from 'superagent'
 
-export function getGreeting() {
-  return request.get('/greeting').then((res) => res.body.greeting)
-}
+// export function getGreeting() {
+//   return request.get('/greeting').then((res) => res.body.greeting)
+// }
---
+++ client/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Nav() {
+  return (
+    <div className="nav">
+      <a href="/">
+        <img className="logo" src="/images/hbb-logo.png" alt="Logo" />
+      </a>
+    </div>
+  )
+}
+
+export default Nav
