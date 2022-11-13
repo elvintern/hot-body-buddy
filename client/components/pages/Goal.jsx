40087deb27fe4a7a2030ae8267b68e650d2b1d3d
elvintern <elvintern@gmail.com>
2022-11-13 19:45:39 +1300

    Add goal page

--- client/components/pages/Goal.jsx
+++ client/components/pages/Goal.jsx
@@ -4,8 +4,22 @@ import Quote from '../items/Quote'
 export default function Goal() {
   return (
     <>
-      <p>Goal?</p>
       <Quote />
+      <form className="form form-goal">
+        <label htmlFor="userGoal" className="form__label">
+          tell me your goal
+        </label>
+        <input
+          type="textarea"
+          maxLength={30}
+          name="userGoal"
+          id="userGoal"
+          className="form__input"
+          required
+        />
+
+        <button className="btn btn-next">next</button>
+      </form>
     </>
   )
 }
