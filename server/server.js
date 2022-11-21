a0ea7764a9ccc187fd45556d779530f2aa8ff470
elvintern <elvintern@gmail.com>
2022-11-21 22:57:34 +1300

    Add an universal server

--- server/server.js
+++ server/server.js
@@ -12,4 +12,8 @@ server.use(express.static(path.join(__dirname, './public')))
 server.use('/api/v1/fruits', fruitsRouter)
 server.use('/api/v1/users', userRouter)
 
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
 module.exports = server
